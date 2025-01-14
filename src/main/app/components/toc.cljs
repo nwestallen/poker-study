fa643b729123f8f630ee0dbcbcdd2540baf0f62f
Nick <nwestallen@gmail.com>
2025-01-13 18:28:21 -0800

    render LaTeX

--- src/main/app/components/toc.cljs
+++ src/main/app/components/toc.cljs
@@ -5,28 +5,13 @@
             ["react-dom/client" :as rdom]
             [clojure.walk :as walk]))
 
-(def toc-tree
-  (d/ul {:class-name "list-none list-inside bg-[#3787B4] text-neutral-100 pr-10 pl-5 mr-5 pt-5"}
-   (d/li
-    (d/a {:href "#section_1" :class-name "font-semibold"} "Section 1")
-    (d/ul {:class-name "list-none indent-2"}
-     (d/li (d/a {:class-name "font-medium"} "Subsection A"))
-     (d/li (d/a "Subsection B"))
-     )
-    )
-   (d/li {:href "#bottom"} "Bottom Section")
-   )
-  )
-
-(defnc toc [content] toc-tree)
-
 (def indentmap
   {"d/h2" "indent-0" "d/h3" "indent-2"})
 (def stylemap
   {"d/h2" "font-semibold" "d/h3" "font-medium"})
 
 (defnc TableOfContents [{:keys [headers]}]
-  (d/nav {:class-name "bg-[#3787B4] text-neutral-100 pr-10 pl-5 mr-5 pt-5"}
+  (d/nav {:class-name "bg-sky-600 text-neutral-100 pr-10 pl-5 mr-5 pt-5"}
     (d/ul {:class-name "list-none list-inside"}
       (for [[tag atts text] headers]
         (d/li {:class-name (indentmap (str tag))}
--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -4,14 +4,10 @@
             [helix.dom :as d]
             ["react-dom/client" :as rdom]
             [app.components.toc :refer [TableOfContents]]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            ["react-katex" :refer [InlineMath BlockMath]]
+            )
   (:require-macros [app.utils.macros :refer [defpage]]))
-
-(defnc greeting
-  "A component which greets a user."
-  [{:keys [name]}]
-  ;; use helix.dom to create DOM elements
-  (d/div {:class-name "px-5"} "Hello, " (d/strong name) "!"))
 
 (defn extract-headers [form]
   (let [headers (atom [])]
@@ -25,24 +21,23 @@
     @headers))
 
 (def outline-style
-  {:h2 "font-bold indent-2 text-xl text-red-600"
-   :h3 "font-semibold indent-4"})
+  {:h2 "font-bold indent-2 text-2xl text-slate-600 my-2"
+   :h3 "font-semibold indent-4 text-xl text-slate-500 my-1"})
 
 (defpage PageContent
-  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
     (d/div
-           (d/h1 {:class-name "text-4xl font-bold py-5"} "♠" (d/span {:class "text-red-600"} "♥︎ ") "Understanding Poker Theory " (d/span {:class "text-red-600"} "♦︎") "♣︎")
-           (d/a {:href "#bottom"} "jump to bottom of page")
-           ($ greeting {:name (:name state)})
-           (d/input {:class-name "mx-5 my-1 border border-black"
-                     :value (:name state)
-                     :on-change #(set-state assoc :name (.. % -target -value))})
-      (d/h2 {:class-name (:h2 outline-style)}"Section 1")
+           (d/h1 {:class-name "text-4xl font-bold py-5"} "♠" (d/span {:class "text-red-600"} "♥︎ ") "Understanding Poker Theory " (d/span {:class "text-sky-600"} "♦︎") (d/span {:class "text-green-500"}"♣︎"))
+      (d/h2 {:id "hand-rankings" :class-name (:h2 outline-style)} "Hand Rankings")
            (d/h3 {:id "suba" :class-name (:h3 outline-style)} "Subsection A")
-           (d/p "I'm gonna put some more text here")
+           (d/p {:class-name "indent-6"}"I'm gonna put some more text here")
       (d/h3 {:id "subB" :class-name (:h3 outline-style)} "Subsection B")
-           (d/p "more text here")
-           (d/h2 {:id "bottom" :class-name (:h2 outline-style)} "Section 2"))))
+           (d/p {:class-name "indent-6"}"more text here")
+           (d/h2 {:id "bottom" :class-name (:h2 outline-style)} "Section 2")
+      ($ InlineMath "\\frac{a}{b} \\cdot \\frac{c}{d} = \\frac{ac}{bd}")
+      ($ BlockMath {:math "\\int_0^\\infty x^2 dx"})
+      ))
+
+($ InlineMath {:math "\\int_0^\\infty x^2 dx"})
 
 (defnc app []
   {:helix/features {:fast-refresh true}}
