36b54f43718bb69141c66151e5f285512e45fa0c
Nick <nwestallen@gmail.com>
2025-01-15 10:13:48 -0800

    table of contents styling

--- src/main/app/components/contents.cljs
+++ src/main/app/components/contents.cljs
@@ -7,12 +7,13 @@
             [clojure.walk :as walk]))
 
 (def indentmap
-  {"d/h2" (css :pl-0) "d/h3" (css :pl-2)})
+  {"d/h2" (css :pl-0 :mt-5) "d/h3" (css :pl-2)})
 (def stylemap
   {"d/h2" (css :font-semibold) "d/h3" (css :font-medium)})
 
 (defnc TableOfContents [{:keys [headers]}]
-  (d/nav {:class-name (css :bg-sky-600 :text-neutral-100 :pr-10 :pl-5 :mr-5 :pt-5)}
+  (d/nav {:class-name (css :bg-sky-600 :text-neutral-100 :px-6 :mr-12 :pt-5 :border-solid :border-r-2 :border-sky-300)}
+    (d/h1 {:class-name (css :font-semibold :underline)}"Table of Contents")
     (d/ul {:class-name (css :list-none :list-inside)}
       (for [[tag atts text] headers]
         (d/li {:class-name (indentmap (str tag))}
