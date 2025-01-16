66001ed033d589e3a64644b3726c218a8ecfc63c
Nick <nwestallen@gmail.com>
2025-01-15 18:09:10 -0800

    fill hand ranks table

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -33,10 +33,16 @@
       (d/h2 {:id "hand-rankings" :class-name (:h2 outline-style)} "Hand Rankings")
       ($ Table {:headers ["Rank" "Hand Name" "Form" "5 Card Combos" "7 Card Combos"]
                 :rows [
-                       [1 "Royal Flush" (hand-text "As" "Ks" "Qs" "Js" "Ts") ($ BlockMath {:math "\\binom{4}{1} = 4"}) ($ BlockMath {:math "\\binom{4}{1}\\binom{47}{2} = 4\\,324"})]
-                       [2 "Straight Flush" (hand-text "9h" "8h" "7h" "6h" "5h") ($ BlockMath {:math "\\binom{9}{1}\\binom{4}{1} = 36"}) ($ BlockMath {:math "\\binom{9}{1}\\binom{4}{1}\\binom{47}{2} = 37\\,260"})]
-                       [3 "Four of a Kind" (hand-text "Qs" "Qh" "Qc" "Qd" "Kc") ($ BlockMath {:math "\\binom{13}{1}\\binom{4}{1}\\binom{12}{1}\\binom{4}{1} = 156"}) ($ BlockMath {:math "\\binom{13}{1}\\binom{48}{3} = 224\\,848"})]
-                       [3 "Four of a Kind" (hand-text "Qs" "Qh" "Qc" "Qd" "Kc") ($ BlockMath {:math "\\binom{13}{1}\\binom{4}{1}\\binom{12}{1}\\binom{4}{1} = 156"}) ($ BlockMath {:math "\\binom{13}{1}\\binom{48}{3} = 224\\,848"})]
+                       [1 "Royal Flush" (hand-text "As" "Ks" "Qs" "Js" "Ts") ($ BlockMath {:math "\\binom{4}{1} = 4"}) ($ BlockMath {:math "4\\,324"})]
+                       [2 "Straight Flush" (hand-text "9h" "8h" "7h" "6h" "5h") ($ BlockMath {:math "\\binom{9}{1}\\binom{4}{1} = 36"}) ($ BlockMath {:math "37\\,260"})]
+                       [3 "Four of a Kind" (hand-text "Qs" "Qh" "Qc" "Qd" "Kc") ($ BlockMath {:math "\\binom{13}{1}\\binom{4}{1}\\binom{12}{1}\\binom{4}{1} = 624"}) ($ BlockMath {:math "224\\,848"})]
+                       [4 "Full House" (hand-text "Ks" "Kh" "Kd" "9s" "9h") ($ BlockMath {:math "\\binom{13}{1}\\binom{4}{3}\\binom{12}{1}\\binom{4}{2} = 3\\,744"}) ($ BlockMath {:math "3\\,473\\,184"})]
+                       [5 "Flush" (hand-text "Kc" "Jc" "9c" "8c" "4c") ($ BlockMath {:math "\\binom{13}{5}\\binom{4}{1}-\\binom{10}{1}\\binom{4}{1} = 5\\,108"}) ($ BlockMath {:math "4\\,047\\,644"})]
+                       [6 "Straight" (hand-text "6c" "5h" "4s" "3h" "2d") ($ BlockMath {:math "\\binom{10}{1}\\binom{4}{1}^5-\\binom{10}{1}\\binom{4}{1} = 10\\,200"}) ($ BlockMath {:math "6\\,180\\,020"})]
+                       [7 "Three of a Kind" (hand-text "7d" "7c" "7h" "As" "Qh") ($ BlockMath {:math "\\binom{13}{1}\\binom{4}{3}\\binom{12}{2}\\binom{4}{1}^2 = 54\\,912"}) ($ BlockMath {:math "6\\,461\\,120"})]
+                       [8 "Two Pair" (hand-text "As" "Ac" "8s" "8d" "Js") ($ BlockMath {:math "\\binom{13}{2}\\binom{4}{1}^2\\binom{11}{1}\\binom{4}{1} = 123\\,552"}) ($ BlockMath {:math "31\\,433\\,400"})]
+                       [9 "One Pair" (hand-text "Th" "Td" "Kc" "Qc" "4h") ($ BlockMath {:math "\\binom{13}{1}\\binom{4}{2}\\binom{12}{3}\\binom{4}{1}^3 = 1\\,098\\,240"}) ($ BlockMath {:math "58\\,627\\,800"})]
+                       [10 "High Card" (hand-text "Kd" "Jh" "9d" "7c" "6c") ($ BlockMath {:math "\\left[\\binom{13}{5}-\\binom{10}{1}\\right]\\left[\\binom{4}{1}^5 - \\binom{4}{1}\\right] = 1\\,302\\,540"}) ($ BlockMath {:math "23\\,294\\,460"})]
                        ]})
            (d/h3 {:id "suba" :class-name (:h3 outline-style)} "Subsection A")
            (d/p {:class-name (css :pl-6)}"I'm gonna put some more text here, maybe talk about " (hand-text "Ah" "Kd"))
