2566c2a18dde33d4cecd2f6bbd366e584ca0e7d8
Nick <nwestallen@gmail.com>
2025-06-19 12:28:46 -0700

    cardchart cells height for partial ranges

--- src/main/app/components/cardchart.cljs
+++ src/main/app/components/cardchart.cljs
@@ -9,19 +9,22 @@
 
 (def ranks '("A" "K" "Q" "J" "T" "9" "8" "7" "6" "5" "4" "3" "2"))
 
+(defn pct [n d]
+  (* 100 (/ n d)))
+
 (defnc Cardsquare [{:keys [hand strategy set-strategy update-strat paint] {:keys [raise call fold]} :act}]
   (let [
-        [state set-state] (hooks/use-state {:r (str raise) :c (str call) :f (str fold) :rc (str (+ raise call))})
+        [state set-state] (hooks/use-state {:r raise :c call :f fold :rc (+ raise call) :h (+ raise call fold)})
         paint-square (fn [] (set-strategy ((strat-pipe strategy) {(edn/read-string update-strat) (list hand)})))
         ]
-    (hooks/use-effect [strategy] (set-state {:r (str raise) :c (str call) :f (str fold) :rc (str (+ raise call))}))
+    (hooks/use-effect [strategy] (set-state {:r (str raise) :c (str call) :f (str fold) :rc (str (+ raise call)) :h (+ raise call fold)}))
     (d/svg {:viewBox "0 0 100 100" :width "100%" :height "100%" :xlmns "http://www.w3.org/2000/svg" :on-mouse-down #(paint-square) :on-mouse-enter #(if paint (paint-square))}
            (d/rect {:width "100%" :height "100%" :x "0" :y "0" :rx "10" :ry "10" :fill "rgb(64 64 64)"})
            (d/defs (d/clipPath {:id "rounded-corners"} (d/rect {:x "0" :y "0" :width "100" :height "100" :rx "10" :ry "10"})))
            (d/g {:clipPath "url(#rounded-corners)"}
-                (d/rect {:width (:r state) :height"100" :x "0" :y "0" :fill "rgb(239 68 68)"})
-                (d/rect {:width (:c state) :height"100" :x (:r state) :y "0" :fill "rgb(34 197 94)"})
-                (d/rect {:width (:f state) :height"100" :x (:rc state) :y "0" :fill "rgb(14 165 233)"})
+                (d/rect {:width (pct (:r state) (:h state)) :height (:h state) :x "0" :y (- 100 (:h state)) :fill "rgb(239 68 68)"})
+                (d/rect {:width (pct (:c state) (:h state)) :height (:h state) :x (pct (:r state) (:h state)) :y (- 100 (:h state)) :fill "rgb(34 197 94)"})
+                (d/rect {:width (pct (:f state) (:h state)) :height (:h state) :x (pct (:rc state)(:h state)) :y (- 100 (:h state)) :fill "rgb(14 165 233)"})
          )
          (d/text {:x "50" :y "50" :textAnchor "middle" :dominantBaseline "middle" :fill "white" :fontSize "36" :fontWeight "500" :class-name (css :select-none)} hand)
          )))
