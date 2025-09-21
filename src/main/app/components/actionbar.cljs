b3f04deedd6a90e39bc7b59ea777f9bd38e2289a
Nick <nwestallen@gmail.com>
2025-09-21 15:46:30 -0700

    actionbar flat interior borders, resizing

--- src/main/app/components/actionbar.cljs
+++ src/main/app/components/actionbar.cljs
@@ -1,5 +1,5 @@
 (ns app.components.actionbar
-  (:require [helix.core :refer [defnc $]]
+  (:require [helix.core :refer [defnc]]
             [helix.dom :as d]))
 
 (defnc ActionBar [{:keys [summary]}]
@@ -7,9 +7,17 @@
         raise-width (if (> total 0) (* 100 (/ (:raise summary 0) total)) 0)
         call-width (if (> total 0) (* 100 (/ (:call summary 0) total)) 0)
         fold-width (if (> total 0) (* 100 (/ (:fold summary 0) total)) 0)]
-    (d/svg {:width "100%" :height "20" :class-name "mt-2"}
-           (d/rect {:x 0 :y 0 :width (str raise-width "%") :height 20 :fill "rgb(239 68 68)" :rx "3"}) ; red-500
-           (d/rect {:x (str raise-width "%") :y 0 :width (str call-width "%") :height 20 :fill "rgb(34 197 94)"}) ; green-500
-           (d/rect {:x (str (+ raise-width call-width) "%") :y 0 :width (str fold-width "%") :height 20 :fill "rgb(14 165 233)" :rx "3"}))))
+    (d/div {:style {:width "calc(14 * 7% + 14 * 1px)"}}
+           (d/svg {:width "100%" :height "20" :class-name "mt-2"}
+                  (d/defs
+                    (d/clipPath {:id "rounded-bar"}
+                      (d/rect {:x "0" :y "0" :width "100%" :height "20" :rx "3" :ry "3"})))
+                  (d/g {:clipPath "url(#rounded-bar)"}
+                    (when (> raise-width 0)
+                      (d/rect {:x 0 :y 0 :width (str raise-width "%") :height 20 :fill "rgb(239 68 68)"}))
+                    (when (> call-width 0)
+                      (d/rect {:x (str raise-width "%") :y 0 :width (str call-width "%") :height 20 :fill "rgb(34 197 94)"}))
+                    (when (> fold-width 0)
+                      (d/rect {:x (str (+ raise-width call-width) "%") :y 0 :width (str fold-width "%") :height 20 :fill "rgb(14 165 233)"})))))))
 
 
--- src/main/app/components/paintchart.cljs
+++ src/main/app/components/paintchart.cljs
@@ -1,25 +1,20 @@
 (ns app.components.paintchart
-  (:require [helix.core :refer [defnc $ <>]]
+  (:require [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]
             [shadow.css :refer [css]]
             [app.components.cardchart :refer [PureChart]]
-            [app.components.rangeform :refer [RangeForm]]
-            [app.components.accreport :refer [AccReport]]
-            [app.components.freqchart :refer [FreqChart]]
-            [app.components.pokertable :refer [TableContainer]]
             [app.components.actionbar :refer [ActionBar]]
-            [app.components.mixslider :refer [SliderSquare]]
-            [app.utils.strategy :refer [action-summary all-fold convert-ranges strat-accuracy simplify-strat abbrv-strat percent-summary]]
+            [app.utils.strategy :refer [percent-summary]]
             ["react-dom/client" :as rdom]))
 
 (defnc Paintchart [{:keys [strategy set-strategy! mix height update]}]
   (let [summary (hooks/use-memo [strategy] (percent-summary strategy))]
-    (d/div {:class-name (css :flex :flex-col :w-full :aspect-square)}
+    (d/div {:class-name (css :flex :flex-col :w-full :p-0.5)}
            (d/div {:class-name (css :flex :flex-row :mb-2 :text-shadow-sm)}
                   (d/div {:class-name (css :w-5 :h-5 :rounded-full :bg-red-500 :mx-1 :mt-0.5 :shadow-md)}) (d/p {:class-name (css :font-semibold :mr-2)} "Raise")
                   (d/div {:class-name (css :w-5 :h-5 :rounded-full :bg-green-500 :mx-1 :mt-0.5 :shadow-md)}) (d/p {:class-name (css :font-semibold :mr-2)} "Call")
                   (d/div {:class-name (css :w-5 :h-5 :rounded-full :bg-sky-500 :mx-1 :mt-0.5 :shadow-md)}) (d/p {:class-name (css :font-semibold :mr-2)} "Fold"))
                   ($ PureChart {:strategy strategy :set-strategy set-strategy! :update-strat update})
-                  (d/div {:class-name (css :my-1 :w-full :shadow-md)} ($ ActionBar {:summary summary})))))
+                  (d/div {:class-name (css :my-1 :shadow-md)} ($ ActionBar {:summary summary})))))
 
